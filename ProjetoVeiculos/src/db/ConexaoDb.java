bf96d9682dd8961322f31e87d58c81c511e6abf2
Nelson de Oliveira <nelsonjrgg@gmail.com>
2025-11-17 19:22:17 -0300

    Implementando classe para conexão com o banco de dados

---
+++ ProjetoVeiculos/src/db/ConexaoDb.java
@@ -0,0 +1,23 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoDb {
+
+    private static final String URL =
+            "jdbc:postgresql://ep-rapid-forest-ad89bsmb-pooler.c-2.us-east-1.aws.neon.tech/neondb";
+
+    private static final String USER = "neondb_owner";
+    private static final String PASSWORD = "npg_XFrs9d6jiMZP";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao conectar ao banco: " + e.getMessage());
+        }
+    }
+}
+
